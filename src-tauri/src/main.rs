97fde6ce62c0a5b44a4b26339f3af7c29c843187
Jhonata Carvalho <jhonatacarvalho@MacBook-Air-de-Jhonata.local>
2026-08-18 21:14:33 -0300

    ci: fix tauri crate versions and add frontend build step

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -59,6 +59,7 @@ fn main() {
     };
 
     tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
         .manage(app_state)
         .invoke_handler(tauri::generate_handler![
             get_local_ip,
